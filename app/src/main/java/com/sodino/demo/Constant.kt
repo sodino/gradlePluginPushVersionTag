d01aa06579822bb2a122488a14425747512e2b1f
cht <cht@meitu.com>
2018-03-24 11:32:23 +0800

    init project

---
+++ app/src/main/java/com/sodino/demo/Constant.kt
@@ -0,0 +1,9 @@
+package com.sodino.demo
+
+/**
+ * Created by sodino on 2018/3/24.
+ */
+object Version {
+    const val name = "1.2.0"
+    const val code = 10200
+}
---
+++ buildSrc/src/main/groovy/sodino/git/PushVersion.groovy
@@ -0,0 +1,12 @@
+package sodino.git
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+public class PushVersion implements Plugin<Project> {
+
+    @Override
+    void apply(Project project) {
+
+    }
+}
