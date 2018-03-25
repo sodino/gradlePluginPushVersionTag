60673659b05cadcfdc6ec665b8d69353b84cda18
cht <cht@meitu.com>
2018-03-25 14:51:28 +0800

    【Version】v2.1.0-beta-09　is　out

--- app/src/main/java/com/sodino/demo/Constant.kt
+++ app/src/main/java/com/sodino/demo/Constant.kt
@@ -5,6 +5,6 @@ package com.sodino.demo
  * Created by sodino on 2018/3/24.
  */
 object Version {
-    const val name = "2.1.0-beta-08" // name
-    const val code = 201000008     // code
+    const val name = "2.1.0-beta-09" // name
+    const val code = 201000009     // code
 }
