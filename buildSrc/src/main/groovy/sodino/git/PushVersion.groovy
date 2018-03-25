6b0afe2359739f9d41c415ab253aee9643c677de
cht <cht@meitu.com>
2018-03-25 15:03:16 +0800

    【Version】v2.1.0-beta-09　is　out

--- buildSrc/src/main/groovy/sodino/git/PushVersion.groovy
+++ buildSrc/src/main/groovy/sodino/git/PushVersion.groovy
@@ -200,11 +200,18 @@ public class PushVersion implements Plugin<Project> {
             // 版本名以'"'结尾，即下方中的正则  '\"$'， 然后替换时要补偿该'"'
             String fixResult = findResult.replaceAll(/[0-9.]+.*\"$/, bean.versionName + "\"")
             String result = line.replaceAll(findResult, fixResult)
+            // 最后加上改动的时间戳
+            result +=  "   // time modified: ${yyyyMMddHHmmssSSS()}"
             println "fix version name from [$line] to [$result]"
             return result
         }
     }
 
+    String yyyyMMddHHmmssSSS() {
+        def date = new Date()
+        def formattedDate = date.format('yyyy-MM-dd HH:mm:ss.SSS')
+        return formattedDate
+    }
 
     def printProjectInfo(Project project) {
         println "project name=" + project.name
