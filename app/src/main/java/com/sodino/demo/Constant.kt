0e1200822aea18b26ffac8b4ea65ade8ecd5a785
cht <cht@meitu.com>
2018-03-26 21:44:08 +0800

    【Version】v1.0.1　is　out

--- app/src/main/java/com/sodino/demo/Constant.kt
+++ app/src/main/java/com/sodino/demo/Constant.kt
@@ -5,6 +5,6 @@ package com.sodino.demo
  * Created by sodino on 2018/3/24.
  */
 object Version {
-    const val name = "1.0.0" // name   // time modified: 2018-03-25 15:03:16.167
-    const val code = 1     // code
+    const val name = "1.0.1" // name   // time modified: 2018-03-25 15:03:16.167
+    const val code = 2     // code
 }
