525fa9d09385907b61a7a73f9837d19ce9e7147a
cht <cht@meitu.com>
2019-04-28 22:19:08 +0800

    【Version】1.0.4　is　out

--- app/src/main/java/com/sodino/demo/Constant.kt
+++ app/src/main/java/com/sodino/demo/Constant.kt
@@ -5,6 +5,6 @@ package com.sodino.demo
  * Created by sodino on 2018/3/24.
  */
 object Version {
-    const val name = "1.0.3" // name
-    const val code = 4     // code
+    const val name = "1.0.4" // name
+    const val code = 5     // code
 }
