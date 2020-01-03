1d3d93d0fb523fa18227be078632799148b48fb1
sodino <sodino@qq.com>
2020-01-03 15:00:28 +0800

    【Version】1.0.0-beta-1 is out

--- app/src/main/java/com/sodino/demo/Constant.kt
+++ app/src/main/java/com/sodino/demo/Constant.kt
@@ -5,6 +5,6 @@ package com.sodino.demo
  * Created by sodino on 2018/3/24.
  */
 object Version {
-    const val name = "1.0.6" // name
+    const val name = "1.0.0-beta-1" // name
     const val code = 106     // code
 }
