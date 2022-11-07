1b947438e43b927eb5cd7f15a453868dda44fe09
sodino <sodino@qq.com>
2022-11-07 16:09:24 +0800

    【Version】1.0.8 is out

--- app/src/main/java/com/sodino/demo/Constant.kt
+++ app/src/main/java/com/sodino/demo/Constant.kt
@@ -5,6 +5,6 @@ package com.sodino.demo
  * Created by sodino on 2018/3/24.
  */
 object Version {
-    const val name = "1.0.7"   // tag : 2020-01-03 16:03:41.744
-    const val code = 107   // tag : 2020-01-03 16:03:41.744
+    const val name = "1.0.8"   // tag : 2021-11-07 16:08:59.732
+    const val code = 108   // tag : 2022-11-07 16:08:59.732
 }
